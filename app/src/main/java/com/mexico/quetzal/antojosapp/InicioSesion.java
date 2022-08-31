4e75ca0031ab2cd05b6c27ce2d82eb05931bf354
Diego Hernandez <quetzal.developer@gmail.com>
2022-08-31 12:41:53 -0500

    Add: GUI. Inicio de sesion

---
+++ app/src/main/java/com/mexico/quetzal/antojosapp/InicioSesion.java
@@ -0,0 +1,14 @@
+package com.mexico.quetzal.antojosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_sesion);
+    }
+}
