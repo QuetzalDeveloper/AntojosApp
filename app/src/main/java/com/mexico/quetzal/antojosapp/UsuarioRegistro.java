e511936162f089f0a600b89ceda1e47aa6d87e3c
Diego Hernandez <quetzal.developer@gmail.com>
2022-08-31 19:45:11 -0500

    Add: GUI. Se creo la activity de registrar usuario

---
+++ app/src/main/java/com/mexico/quetzal/antojosapp/UsuarioRegistro.java
@@ -0,0 +1,14 @@
+package com.mexico.quetzal.antojosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsuarioRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_registro);
+    }
+}
