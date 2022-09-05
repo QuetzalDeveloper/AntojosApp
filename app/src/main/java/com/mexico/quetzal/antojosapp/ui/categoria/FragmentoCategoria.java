e0810c24f3f9788635828e87cc62b263fb49d68f
Diego Hernandez <quetzal.developer@gmail.com>
2022-09-05 12:53:49 -0500

    ADD: GUI. Se empezo a realizar el card dashboard del fragmento categorias

--- app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/FragmentoCategoria.java
+++ app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/FragmentoCategoria.java
@@ -24,8 +24,8 @@ public class FragmentoCategoria extends Fragment {
         binding = FragmentoCategoriaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        viewModelCategoria.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final TextView textView = binding.textGallery;
+        //viewModelCategoria.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
