641a098f61c9fdeab4610427f2f23bd253d55c17
Diego Hernandez <quetzal.developer@gmail.com>
2022-09-05 12:17:11 -0500

    Update: GUI. Inicio. se agrego el recycler view

--- app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/FragmentoCategoria.java
+++ app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/FragmentoCategoria.java
@@ -1,4 +1,4 @@
-package com.mexico.quetzal.antojosapp.ui.gallery;
+package com.mexico.quetzal.antojosapp.ui.categoria;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.mexico.quetzal.antojosapp.databinding.FragmentGalleryBinding;
+import com.mexico.quetzal.antojosapp.databinding.FragmentoCategoriaBinding;
 
-public class GalleryFragment extends Fragment {
+public class FragmentoCategoria extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentoCategoriaBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        ViewModelCategoria viewModelCategoria =
+                new ViewModelProvider(this).get(ViewModelCategoria.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentoCategoriaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        viewModelCategoria.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/ViewModelCategoria.java
+++ app/src/main/java/com/mexico/quetzal/antojosapp/ui/categoria/ViewModelCategoria.java
@@ -1,14 +1,14 @@
-package com.mexico.quetzal.antojosapp.ui.gallery;
+package com.mexico.quetzal.antojosapp.ui.categoria;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class ViewModelCategoria extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public ViewModelCategoria() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
